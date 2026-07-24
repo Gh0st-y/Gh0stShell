42eb20d1d7ce80ef64d3058310ae6c54393ca016
Gh0st-y <ruanvandermerwe208@gmail.com>
2026-07-24 17:37:22 +0200

    Added everything together

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#define _GNU_SOURCE
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,14 +11,91 @@
 #define SHELL_TOK_BUFSIZE 64
 #define SHELL_TOK_DELIM " \t\r\n\a"
 
+// Forward declarations for built-in shell commands
+int shell_cd(char **args);
+int shell_help(char **args);
+int shell_exit(char **args);
+
+// String array of built-in command names
+char *builtin_str[] = 
+{
+    "cd",
+    "help",
+    "exit"
+};
+
+// Array of matching function pointers
+int (*builtin_func[]) (char **) =
+{
+    &shell_cd,
+    &shell_help,
+    &shell_exit
+};
+
+int shell_num_builtins(void)
+{
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+/* Built-in implementations */
+
+int shell_cd(char **args)
+{
+    if (args[1] == NULL)
+    {
+        // If no argument is passed, default to HOME directory
+        char *home = getenv("HOME");
+        if (home == NULL)
+        {
+            fprintf(stderr, "spook :> expected argument to \"cd\"\n");
+        }
+        else 
+        {
+            if (chdir(home) != 0)
+            {
+                perror("spook");
+            }
+        }
+    }
+    else
+    {
+        if (chdir(args[1]) != 0)
+        {
+            perror("spook");
+        }
+    }
+    return 1; // Keep running
+}
+
+int shell_help(char **args)
+{
+    (void)args; // Silence unused variable compiler warnings
+    printf("Custom C Unix Shell\n");
+    printf("Type program names and arguments, then hit enter.\n");
+    printf("The following commands are built in:\n");
+
+    for (int i = 0; i < shell_num_builtins(); i++)
+    {
+        printf("  %s\n", builtin_str[i]);
+    }
+
+    printf("Use the 'man' command for information on other programs.\n");
+    return 1;
+}
+
+int shell_exit(char **args)
+{
+    (void)args;
+    return 0; // Returning 0 will break our REPL loop and exit!
+}
+
+/* Dispatcher function */
 int shell_launch(char **args)
 {
     pid_t pid;
-    pid_t wpid;
     int status;
 
     pid = fork();
-
     if (pid == 0)
     {
         // --- CHILD PROCESS ---
@@ -37,12 +117,59 @@ int shell_launch(char **args)
         // Wait until child process finishes state change
         do 
         {
-            wpid = waitpid(pid, &status, WUNTRACED);
+            waitpid(pid, &status, WUNTRACED);
         }
         while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
 
     return 1; // Return 1 to continue the shell loop to keep runnning
+}
+
+int shell_execute(char **args)
+{
+    if (args[0] == NULL)
+    {
+        // An empty command was entered (user just hit enter)
+        return 1;
+    }
+
+    // Check for built-in commands first
+    for (int i = 0; i < shell_num_builtins(); i++)
+    {
+        if (strcmp(args[0], builtin_str[i]) == 0)
+        {
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    // If not a built-in, run as an external program
+    return shell_launch(args);
+}
+
+char *shell_read_line(void)
+{
+    char *line = NULL;
+    size_t bufsize = 0; // getline allocates memory automatically
+
+    if (getline(&line, &bufsize, stdin) == -1)
+    {
+        if (feof(stdin))
+        {
+            // EOF (Ctrl+D) reached
+            printf("\n");
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            perror("spook :> read line error");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Remove trailing newline AND carriage return characters
+    line[strcspn(line, "\r\n")] = '\0';
+
+    return line;
 }
 
 // Split the input line into tokens
@@ -89,53 +216,31 @@ char **shell_split_line(char *line)
 // Handles the shells input loop
 void shell_loop(void)
 {
-    char *line = NULL;
-    size_t bufsize = 0; // getline allocates memory automatically when bufsize is 0
+    char *line;
     char **args;
+    int status;
 
-    while (1)
+    do
     {
         printf("spook :> ");
-        fflush(stdout); // Ensure the prompt prints immediately
-        
-        // Read line from stdin
-        if (getline(&line, &bufsize, stdin) == -1)
-        {
-            // Handle EOF or error
-            if (feof(stdin))
-            {
-                printf("\n");
-                exit(EXIT_SUCCESS);
-            }
-            else
-            {
-                perror("getline");
-                exit(EXIT_FAILURE);
-            }
-        }
+        fflush(stdout);
 
-        // split line into argument array
+        line = shell_read_line();
         args = shell_split_line(line);
+        status = shell_execute(args);
 
-        // Debug print to test tokenization
-        if (args[0] != NULL)
-        {
-            printf("Command: %s\n", args[0]);
-            for (int i = 1; args[i] != NULL; i++)
-            {
-                printf("  Arg[%d]: %s\n", i, args[i]);
-            }
-        }
-
-        // free token array (line buffer free is handled by getline reuse)
-    }
-
-    free(line);
+        // Free dynamically allcoated memory for the iteration
+        free(line);
+        free(args);
+    } while (status);
 }
 
 int main(int argc, char **argv)
 {
     // Load config files if any
+
+    (void)argc;
+    (void)argv;
 
     // Run command loop
     shell_loop();
